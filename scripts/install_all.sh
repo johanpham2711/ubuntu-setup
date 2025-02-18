f6e771a197ba260209c0bcc11f75a0c5b211796a
Johan Pham <johanpham2711@gmail.com>
2025-02-18 09:37:51 +0700

    script: add script install vscode

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -3,5 +3,6 @@ set -e
 
 bash scripts/install_docker.sh
 bash scripts/install_nvm_node.sh
+bash scripts/install_vscode.sh
 
 echo "All software installed successfully."
---
+++ scripts/install_vscode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Installing VS Code..."
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
+rm -f packages.microsoft.gpg
+
+sudo apt update
+sudo apt install -y code
+
+echo "VS Code installation completed."
